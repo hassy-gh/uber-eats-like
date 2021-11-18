636284ad24a56a75bb6ed71bd931243456e1a929
橋本 脩平 <hashimotoshuhei1@hashimotonoMacBook-Air.local>
2021-11-18 15:44:37 +0900

    Initialize repository

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+3.times do |n|
+  restaurant = Restaurant.new(
+    name: "testレストラン_#{n}",
+    fee: 100,
+    time_required: 10
+  )
+
+  12.times do |m|
+    restaurant.foods.build(
+      name: "フード_#{m}",
+      price: 500,
+      description: "フード_#{m}の説明文です"
+    )
+  end
+
+  restaurant.save!
+end
+
